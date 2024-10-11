c532e101efe362b8621392531c8628bc1188c2bf
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-10-10 23:48:55 -0400

    Add GitHub Pages SPA redirect workaround

---
+++ gatsby-ssr.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export function onRenderBody({ setHeadComponents }) {
+  setHeadComponents([
+    <script
+      key="redirect-script"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function() {
+            var redirect = sessionStorage.redirect;
+            delete sessionStorage.redirect;
+            if (redirect && redirect !== location.href) {
+              history.replaceState(null, null, redirect);
+            }
+          })();
+        `,
+      }}
+    />,
+  ])
+}
